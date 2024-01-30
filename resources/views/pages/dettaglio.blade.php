124eba720ab4e92db26d800dc6d05467c3587208
Vincenzo Tito <vincenzotito@icloud.com>
2024-01-30 19:45:25 +0100

    test

--- resources/views/pages/dettaglio.blade.php
+++ resources/views/pages/dettaglio.blade.php
@@ -11,38 +11,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li id="navlink" class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Home</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <a class="nav-link" href="{{ route('articoli.index') }}">Articoli</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <a class="nav-link" href="{{ route('contacts') }}">Contatti</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <div class="dropdown">
-                    <button id="btnNav" class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Categorie
-                    </button>
-                    <ul class="dropdown-menu">
-                        @foreach ($categorie as $categoria)
-                        <li><a class="dropdown-item" href="{{ route('articoli.category', $categoria)}}">{{$categoria}}</a></li>
-                        @endforeach
-                    </ul>
-                  </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
+  <x-navbar/>
+  
     <a class="tornaInArticoli" href="{{ route('articoli.index') }}">Torna nella Sezione Articoli</a>
     <div class="card" style="width: 60rem; height: 15rem">
         <div class="card-body">
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,57 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    @vite(['resources/css/home.css'])
+    <x-slot:title>Home</x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/home.css">
-</head>
+    <h1>{{$titolo}}</h1>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li id="navlink" class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Home</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <a class="nav-link" href="{{ route('articoli.index') }}">Articoli</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <a class="nav-link" href="{{ route('contacts') }}">Contatti</a>
-              </li>
-              <li id="navlink"  class="nav-item">
-                <div class="dropdown">
-                    <button id="btnNav" class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Categorie
-                    </button>
-                    <ul class="dropdown-menu">
-                        @foreach ($categorie as $categoria)
-                        <li><a class="dropdown-item" href="{{ route('articoli.category', $categoria)}}">{{$categoria}}</a></li>
-                        @endforeach
-                    </ul>
-                  </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-    <header>
-        <h1>{{ $titolo }}</h1>
-    </header>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-</body>
-
-
-</html>
+</x-layout>
